f5f1d641b4ee35c0dd7d4e59dbd644b4cfa32584
samwhelp <samwhelp@users.noreply.github.com>
2025-05-01 17:18:39 +0800

    fix

---
+++ prototype/main/openbox-config/full/Main/asset/overlay/etc/skel/.config/tint2/themes/nordic-bottom-panel/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+THE_BASE_DIR_PATH="$(cd -- "$(dirname -- "$0")" ; pwd)"
+
+
+tint2 -c ${THE_BASE_DIR_PATH}/nordic-bottom.tint2rc &
+#tint2 -c ${THE_BASE_DIR_PATH}/nordic-launcher.tint2rc &
